16baeeaa2dbfc6c7d78bb08c86f243cc34462729
Jon Huber <hubermjonathan@gmail.com>
2019-09-07 04:05:26 -0400

    rewrote checks to be smaller

--- set.py
+++ set.py
@@ -1,5 +1,7 @@
 from data import data1, data2
 from constants import *
+import timeit
+start = timeit.default_timer()
 
 # arrays to keep track of attributes
 red = []
@@ -16,7 +18,7 @@ num2 = []
 num3 = []
 
 # which sample data to use
-data = data2
+data = data1
 
 # function to find sets
 def find_sets():
@@ -24,133 +26,55 @@ def find_sets():
   for i in range(0, 12):
     for j in range(i+1, 12):
       for k in range(j+1, 12):
-        print("checking", i, j, k)
 
-        # count how many of the 3 are in the red array
-        red_count = 0
-        if i in red:
-          red_count += 1
-        if j in red:
-          red_count += 1
-        if k in red:
-          red_count += 1
-
-        # count how many of the 3 are in the green array
-        green_count = 0
-        if i in green:
-          green_count += 1
-        if j in green:
-          green_count += 1
-        if k in green:
-          green_count += 1
-
-        # count how many of the 3 are in the purple array
-        purple_count = 0
-        if i in purple:
-          purple_count += 1
-        if j in purple:
-          purple_count += 1
-        if k in purple:
-          purple_count += 1
-
-        # check if any of the counters are 2
-        if red_count == 2 or green_count == 2 or purple_count == 2:
+        # check if red has any combo of 2
+        if (i in red and j in red and k not in red) or (i in red and j not in red and k in red) or (i not in red and j in red and k in red):
           continue
 
-        # count how many of the 3 are in the squiggle array
-        squiggle_count = 0
-        if i in squiggle:
-          squiggle_count += 1
-        if j in squiggle:
-          squiggle_count += 1
-        if k in squiggle:
-          squiggle_count += 1
-
-        # count how many of the 3 are in the circle array
-        circle_count = 0
-        if i in circle:
-          circle_count += 1
-        if j in circle:
-          circle_count += 1
-        if k in circle:
-          circle_count += 1
-
-        # count how many of the 3 are in the diamond array
-        diamond_count = 0
-        if i in diamond:
-          diamond_count += 1
-        if j in diamond:
-          diamond_count += 1
-        if k in diamond:
-          diamond_count += 1
-
-        # check if any of the counters are 2
-        if squiggle_count == 2 or circle_count == 2 or diamond_count == 2:
+        # check if green has any combo of 2
+        if (i in green and j in green and k not in green) or (i in green and j not in green and k in green) or (i not in green and j in green and k in green):
           continue
 
-        # count how many of the 3 are in the solid array
-        solid_count = 0
-        if i in solid:
-          solid_count += 1
-        if j in solid:
-          solid_count += 1
-        if k in solid:
-          solid_count += 1
-
-        # count how many of the 3 are in the striped array
-        striped_count = 0
-        if i in striped:
-          striped_count += 1
-        if j in striped:
-          striped_count += 1
-        if k in striped:
-          striped_count += 1
-
-        # count how many of the 3 are in the blank array
-        blank_count = 0
-        if i in blank:
-          blank_count += 1
-        if j in blank:
-          blank_count += 1
-        if k in blank:
-          blank_count += 1
-
-        # check if any of the counters are 2
-        if solid_count == 2 or striped_count == 2 or blank_count == 2:
+        # check if purple has any combo of 2
+        if (i in purple and j in purple and k not in purple) or (i in purple and j not in purple and k in purple) or (i not in purple and j in purple and k in purple):
           continue
 
-        # count how many of the 3 are in the num1 array
-        num1_count = 0
-        if i in num1:
-          num1_count += 1
-        if j in num1:
-          num1_count += 1
-        if k in num1:
-          num1_count += 1
-
-        # count how many of the 3 are in the num2 array
-        num2_count = 0
-        if i in num2:
-          num2_count += 1
-        if j in num2:
-          num2_count += 1
-        if k in num2:
-          num2_count += 1
-
-        # count how many of the 3 are in the num3 array
-        num3_count = 0
-        if i in num3:
-          num3_count += 1
-        if j in num3:
-          num3_count += 1
-        if k in num3:
-          num3_count += 1
-
-        # check if any of the counters are 2
-        if num1_count == 2 or num2_count == 2 or num3_count == 2:
+        # check if squiggle has any combo of 2
+        if (i in squiggle and j in squiggle and k not in squiggle) or (i in squiggle and j not in squiggle and k in squiggle) or (i not in squiggle and j in squiggle and k in squiggle):
           continue
 
-        print("FOUND SET")
+        # check if circle has any combo of 2
+        if (i in circle and j in circle and k not in circle) or (i in circle and j not in circle and k in circle) or (i not in circle and j in circle and k in circle):
+          continue
+
+        # check if diamond has any combo of 2
+        if (i in diamond and j in diamond and k not in diamond) or (i in diamond and j not in diamond and k in diamond) or (i not in diamond and j in diamond and k in diamond):
+          continue
+
+        # check if solid has any combo of 2
+        if (i in solid and j in solid and k not in solid) or (i in solid and j not in solid and k in solid) or (i not in solid and j in solid and k in solid):
+          continue
+
+        # check if striped has any combo of 2
+        if (i in striped and j in striped and k not in striped) or (i in striped and j not in striped and k in striped) or (i not in striped and j in striped and k in striped):
+          continue
+
+        # check if blank has any combo of 2
+        if (i in blank and j in blank and k not in blank) or (i in blank and j not in blank and k in blank) or (i not in blank and j in blank and k in blank):
+          continue
+
+        # check if num1 has any combo of 2
+        if (i in num1 and j in num1 and k not in num1) or (i in num1 and j not in num1 and k in num1) or (i not in num1 and j in num1 and k in num1):
+          continue
+
+        # check if num2 has any combo of 2
+        if (i in num2 and j in num2 and k not in num2) or (i in num2 and j not in num2 and k in num2) or (i not in num2 and j in num2 and k in num2):
+          continue
+
+        # check if num3 has any combo of 2
+        if (i in num3 and j in num3 and k not in num3) or (i in num3 and j not in num3 and k in num3) or (i not in num3 and j in num3 and k in num3):
+          continue
+
         set_count += 1
   return set_count
 
@@ -188,10 +112,7 @@ for i in range(0, 12):
   elif data[i][NUMBER] == 3:
     num3.append(i)
 
-print("red array", red)
-print("green array", green)
-print("purple array", purple)
-
 # check for sets
 set_count = find_sets()
-print("set count", set_count)
+stop = timeit.default_timer()
+print("found", set_count, "sets in", ((stop - start)*1000), "ms")
